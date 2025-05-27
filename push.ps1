d7bca85301d4e135ebd053eb4030679ed5495486
Heng_Xin_666 <282000500@qq.com>
2025-05-27 22:50:03 +0800

    新增了OSI七层模型速记

--- push.ps1
+++ push.ps1
@@ -7,11 +7,13 @@ node .\scripts\generateSidebar.js
 # 检查是否有输入参数
 if ($args.Length -eq 0) {
     git add .
+    npx ts-node .\scripts\count-md-words.ts
     # 请输入提交信息, 调用格式为 .\push.ps1 "提交信息"
     Write-Host "Please enter the submission information and call in the format of: .\push.ps1 'commitMsg'"
 } else {
     $commit_message = $args[0]
     Write-Host "commit: $commit_message"
+    git add .
     npx ts-node .\scripts\count-md-words.ts
     git add .
     git commit -m "$commit_message"
