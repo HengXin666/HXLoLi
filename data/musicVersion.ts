45f45b60bfda32494456fe1867b4c3e47dec921a
Heng_Xin[LoLi-Bot] <282000500@qq.com>
2026-03-19 00:19:21 +0800

    [LoLi-Bot]: 🎵 同步 HXLoLi-Music commit_id にゃ♪

--- data/musicVersion.ts
+++ data/musicVersion.ts
@@ -1,2 +1,2 @@
 // HXLoLi-Music 仓库的最新 commit_id (每次音乐仓库更新后需同步修改)
-export const MUSIC_COMMIT_ID = '5e941706d8fc956d31730a0b497a61b80fe4cf33';
+export const MUSIC_COMMIT_ID = '3ae3cff6f0c798eb1671d73e0a9c5473424774f9';
