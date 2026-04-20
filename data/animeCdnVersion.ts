dd857d0c60e50c05bb1a73adcbf562df2161e2a2
Heng_Xin <103022267+HengXin666@users.noreply.github.com>
2026-04-20 17:12:38 +0800

    [LoLi-Bot] 同步 ANiMe: 更新 《Fate/Grand Order -绝对魔兽战线巴比伦尼亚-》

--- data/animeCdnVersion.ts
+++ data/animeCdnVersion.ts
@@ -1,3 +1,3 @@
 // 由 HXLoLi-ANiMe 仓库 GitHub Actions 自动发 PR 更新
 // Tag 格式: HX-{随机6位}-{时间戳}
-export const ANIME_CDN_TAG = 'HX-20260418164836-dWiAcm';
+export const ANIME_CDN_TAG = 'HX-20260420091122-MCy4xj';
