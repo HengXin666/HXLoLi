f727949dbf1a0d736c50e4dc919b834f4f4f8dde
Heng_Xin <103022267+HengXin666@users.noreply.github.com>
2026-05-13 17:50:22 +0800

    [LoLi-Bot] 同步 ANiMe: 更新 《女神“异世界转生想成为什么”我“勇者的肋骨”》

--- data/animeCdnVersion.ts
+++ data/animeCdnVersion.ts
@@ -1,3 +1,3 @@
 // 由 HXLoLi-ANiMe 仓库 GitHub Actions 自动发 PR 更新
 // Tag 格式: HX-{随机6位}-{时间戳}
-export const ANIME_CDN_TAG = 'HX-20260510170019-uqtwfc';
+export const ANIME_CDN_TAG = 'HX-20260513094856-y3u23i';
