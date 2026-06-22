aefe5b799ecd1c05be060a007680d3188b559279
Heng_Xin <103022267+HengXin666@users.noreply.github.com>
2026-06-22 20:55:20 +0800

    [LoLi-Bot] 同步 ANiMe: 更新 《欢迎来到实力至上主义教室 第四季》《关于我转生变成史莱姆这档事 第四季》《女神“异世界转生想成为什么”我“勇者的肋骨”》

--- data/animeCdnVersion.ts
+++ data/animeCdnVersion.ts
@@ -1,3 +1,3 @@
 // 由 HXLoLi-ANiMe 仓库 GitHub Actions 自动发 PR 更新
 // Tag 格式: HX-{随机6位}-{时间戳}
-export const ANIME_CDN_TAG = 'HX-20260618111141-2VRM1o';
+export const ANIME_CDN_TAG = 'HX-20260622125418-PXMdis';
