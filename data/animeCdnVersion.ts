0be1df20586f7cc832778a6f71dab43d603664db
Heng_Xin <103022267+HengXin666@users.noreply.github.com>
2026-08-09 15:57:23 +0800

    [LoLi-Bot] 同步 ANiMe: 更新 《幼女战记 第二季》《无职转生 第三季 ～到了异世界就拿出真本事～》

--- data/animeCdnVersion.ts
+++ data/animeCdnVersion.ts
@@ -1,3 +1,3 @@
 // 由 HXLoLi-ANiMe 仓库 GitHub Actions 自动发 PR 更新
 // Tag 格式: HX-{随机6位}-{时间戳}
-export const ANIME_CDN_TAG = 'HX-20260808163712-v2Dv8K';
+export const ANIME_CDN_TAG = 'HX-20260809075618-APLuCn';
